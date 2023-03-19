8ef732217371f0d9753e47bafc28e6f2287701a4
Karcag Tamas <karcagtamas@outlook.com>
2023-03-19 18:23:41 +0100

    Enhancement of dialogs

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dotnet build -c Release
+
+LIBS=('KarcagS.Shared' 'KarcagS.Common' 'KarcagS.Client.Common' 'KarcagS.Blazor.Common')
+
+if [ -z "$1" ]; then
+    echo >&2 'ERROR: Missing version number'
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo >&2 'ERROR: Missing token'
+    exit 1
+fi
+
+VER=$1
+TOKEN=$2
+
+for l in ${LIBS[@]}; do
+    echo
+    echo "--------$l--------"
+    echo
+
+    dotnet nuget push ./$l/bin/Release/$l.$VER.nupkg --api-key $TOKEN --source https://api.nuget.org/v3/index.json --skip-duplicate
+
+    echo
+    echo "--------$l--------"
+    echo
+done
