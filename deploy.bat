37d83f640c54e54ad46ed218f007cf03f30d5374
Karcag Tamas <karcagtamas@outlook.com>
2025-12-28 11:57:39 +0100

    #1 Add release steps

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,12 @@
 @echo off
 
-dotnet build -c Release
+if "%3" == "minimal" (
+    echo "Minimal deployment is enabled"
+) else (
+    dotnet restore
+    dotent build -c Release --no-restore
+    dotnet pack -c Release --no-build --output ./nupkgs
+)
 
 set libs=KarcagS.Shared KarcagS.API KarcagS.Http KarcagS.API.Auth KarcagS.API.Data KarcagS.API.Export KarcagS.API.Http KarcagS.API.Mail KarcagS.API.Mongo KarcagS.API.Repository KarcagS.API.Shared KarcagS.API.Table KarcagS.Client.Common KarcagS.Blazor.Common
 
@@ -17,18 +23,20 @@ if "%2" == "" (
 set ver=%1
 set token=%2
 
+cd ./nupkgs
+
 (for %%a in (%libs%) do (
     echo.  
     echo --------%%a--------
     echo. 
 
-    cd ./%%a/bin/Release
     dotnet nuget push .\%%a.%ver%.nupkg --api-key %token% --source https://api.nuget.org/v3/index.json --skip-duplicate
-    cd ../../..
     
     echo.  
     echo --------%%a--------
     echo.  
 ))
+
+cd ..
 
 :end
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-dotnet build -c Release
+if [ "$3" = "minimal" ]; then
+    echo "Minimal deployment is enabled"
+else
+    dotnet restore
+    dotent build -c Release --no-restore
+    dotnet pack -c Release --no-build --output ./nupkgs
+fi
 
 LIBS=('KarcagS.Shared' 'KarcagS.API' 'KarcagS.Http' 'KarcagS.API.Auth' 'KarcagS.API.Data' 'KarcagS.API.Export' 'KarcagS.API.Http' 'KarcagS.API.Mail' 'KarcagS.API.Mongo' 'KarcagS.API.Repository' 'KarcagS.API.Shared' 'KarcagS.API.Table' 'KarcagS.Client.Common' 'KarcagS.Blazor.Common')
 
@@ -22,7 +28,7 @@ for l in ${LIBS[@]}; do
     echo "--------$l--------"
     echo
 
-    dotnet nuget push ./$l/bin/Release/$l.$VER.nupkg --api-key $TOKEN --source https://api.nuget.org/v3/index.json --skip-duplicate
+    dotnet nuget push ./nupkgs/$l.$VER.nupkg --api-key $TOKEN --source https://api.nuget.org/v3/index.json --skip-duplicate
 
     echo
     echo "--------$l--------"
